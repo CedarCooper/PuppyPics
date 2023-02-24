e50ece28343207ddf8abb63799f57bffa5b1f24f
Cedar Cooper <cedar.m.cooper@gmail.com>
2023-02-24 14:48:20 -0800

    Image sizing changes

--- src/client/Pages/Confirmation.jsx
+++ src/client/Pages/Confirmation.jsx
@@ -4,6 +4,7 @@ export default function Confirmation() {
 			<h1>You're subscribed!</h1>
 			<h2 id="inbox">Look forward to some fluffy images in your inbox soon!</h2>
 			<img
+				id="dog-gif"
 				src="https://media3.giphy.com/media/m9oewPI16ihGUMZ6ET/200.webp?cid=ecf05e47buv7rrte7ryuf878xuyfzsgk41slkxtzxlkxvtif&rid=200.webp&ct=s"
 				alt="Someone lifting a white and black french bulldog"
 			/>
