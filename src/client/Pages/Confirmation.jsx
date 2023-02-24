d031553707a50bd144a8b94304465b33dd697168
Cedar Cooper <cedar.m.cooper@gmail.com>
2023-02-24 14:06:05 -0800

    added styling and gif

--- src/client/Pages/Confirmation.jsx
+++ src/client/Pages/Confirmation.jsx
@@ -2,6 +2,11 @@ export default function Confirmation() {
 	return (
 		<div className="Confirmed">
 			<h1>You're subscribed!</h1>
+			<h2 id="inbox">Look forward to some fluffy images in your inbox soon!</h2>
+			<img
+				src="https://media3.giphy.com/media/m9oewPI16ihGUMZ6ET/200.webp?cid=ecf05e47buv7rrte7ryuf878xuyfzsgk41slkxtzxlkxvtif&rid=200.webp&ct=s"
+				alt="Someone lifting a white and black french bulldog"
+			/>
 		</div>
 	);
 }
